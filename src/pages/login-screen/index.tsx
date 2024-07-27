5f6cec5d3693457117f92ebc63bfe9894b634ab7
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-27 18:12:15 -0300

    fix: style page

--- src/pages/login-screen/index.tsx
+++ src/pages/login-screen/index.tsx
@@ -2,18 +2,18 @@ import {  User, CircleHelp } from 'lucide-react'
 
 export function LoginScreen(){
     return(
-        <div className="h-screen flex justify-center flex-col bg-gradient-to-t from-red-700 to-red-800 gap-32">
+        <div className="h-screen flex justify-center flex-col bg-gradient-to-t from-red-700 to-red-800 gap-24">
 
-            <div className='w-full flex justify-between mt-7 px-36'>
+            <div className='w-full flex justify-between mt-11 px-28'>
                 <div className='flex space-x-4'>
-                    <User className='size-16 text-zinc-50'/>
-                    <div className='w-px h-16 bg-zinc-100'/>
-                    <h3 className='text-zinc-50 font-bold text-3xl'>Swiss <br /> Wallet</h3>
+                    <User className='size-14 text-zinc-50'/>
+                    <div className='w-px h-14 bg-zinc-100'/>
+                    <h3 className='text-zinc-50 font-bold text-2xl'>Swiss <br /> Wallet</h3>
                 </div>
 
                 <div>
                     <button>
-                        <CircleHelp className='size-14 text-zinc-300'/>
+                        <CircleHelp className='size-10 text-zinc-300'/>
                     </button>
                 </div>
             </div>
