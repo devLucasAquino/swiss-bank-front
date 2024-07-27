294d13d8ecae3ce09ad60b51e17883fc53a94b66
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-27 16:26:59 -0300

    feat: adicionando rotas

---
+++ src/pages/home-page/index.tsx
@@ -0,0 +1,5 @@
+export function HomePage(){
+    return(
+        <h1>Hello HomePage</h1>
+    )
+}
---
+++ src/pages/login-screen/index.tsx
@@ -0,0 +1,5 @@
+export function LoginScreen(){
+    return(
+        <h1>Hello LoginScreen</h1>
+    )
+}
