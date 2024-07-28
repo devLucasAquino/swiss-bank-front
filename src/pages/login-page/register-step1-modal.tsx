46d7307e49d07bbe22d9d9600007e7bb934542ce
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-28 13:57:49 -0300

    feat: created register step 1 modal

---
+++ src/pages/login-page/register-step1-modal.tsx
@@ -0,0 +1,78 @@
+import { ChevronLeft } from "lucide-react";
+
+export function RegisterStep1(){
+    return(
+                    <form className='w-full flex justify-center items-center'>
+                        <div className='flex flex-col space-y-10 bg-white rounded-2xl w-[640px] px-9 py-14'>
+
+                            <div className="space-y-6"> 
+                                <button className='size-12 bg-red-700 rounded-full justify-start'>
+                                    <div className='flex justify-center'>
+                                        <ChevronLeft className='size-8 text-zinc-50'/>
+                                    </div>
+                                </button>
+                                <div>
+                                    <h3 className='text-3xl font-bold font-sans'>Cadastre-se</h3>
+                                    <p className='font-medium text-zinc-600'>Todos os campos são obrigatórios.</p>
+                                </div>
+                            </div>
+
+                            <div className='space-y-3'>
+                                
+
+                            
+                                    <div className='flex flex-col space-y-2'>
+                                        <h2 className='font-bold text-lg font-sans'>Nome completo</h2>
+                                        <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
+                                            type="text"
+                                            placeholder='Insira seu nome completo'    
+                                        />
+                                    </div>
+
+                                    <div className='flex flex-col space-y-2'>
+                                        <h2 className='font-bold text-lg font-sans'>Data de nascimento</h2>
+                                        <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
+                                            type="text"
+                                            placeholder='dd/mm/aaaa'    
+                                        />
+                                    </div>
+
+                                    <div className='flex flex-col space-y-2'>
+                                        <h2 className='font-bold text-lg font-sans'>E-mail</h2>
+                                        <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
+                                            type="text"
+                                            placeholder='Insira seu e-mail'    
+                                        />
+                                    </div>
+
+                                    <div className='flex flex-col space-y-2'>
+                                        <h2 className='font-bold text-lg font-sans'>CPF</h2>
+                                        <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
+                                            type="text"
+                                            placeholder='Insira seu CPF'    
+                                        />
+                                    </div>
+
+                                    <div className='flex flex-col space-y-2'>
+                                        <h2 className='font-bold text-lg font-sans'>Telefone</h2>
+                                        <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
+                                            type="text"
+                                            placeholder='Insira seu telefone'    
+                                        />
+                                    </div>
+
+
+
+                                
+                            </div>
+
+                            <div className='flex justify-center'>
+                                <button className='bg-red-600 py-2 px-20 rounded-xl hover:bg-red-700'>
+                                    <span className='text-zinc-50 font-bold text-lg'>Avançar</span>
+                                </button>
+                            </div>
+
+                        </div>
+                    </form>
+    )
+}
--- src/pages/register-page/index.tsx
+++ src/pages/register-page/index.tsx
@@ -1,76 +1,34 @@
-import { ChevronLeft } from "lucide-react";
+import { CircleHelp, User } from "lucide-react";
+
+import { RegisterStep1 } from "../login-page/register-step1-modal";
 
 export function RegisterPage(){
     return(
-        <form className='w-full flex justify-center items-center p-9'>
-                    <div className='flex flex-col space-y-10'>
+        <div className="h-auto flex justify-center flex-col bg-gradient-to-t from-red-700 to-red-800 gap-24 bg-no-repeat bg-center">
 
-                        <div>
-                            <button className='size-12 bg-red-700 rounded-full justify-start'>
-                                <div className='flex justify-center'>
-                                    <ChevronLeft className='size-8 text-zinc-50'/>
-                                </div>
-                            </button>
-                            <h3 className='text-3xl font-bold font-sans'>Cadastre-se</h3>
-                            <p className='font-medium text-zinc-600'>Todos os campos são obrigatórios.</p>
-                        </div>
+            <div className='w-full flex justify-between mt-11 px-28'>
+                <div className='flex space-x-4'>
+                    <User className='size-14 text-zinc-50'/>
+                    <div className='w-px h-14 bg-zinc-100'/>
+                    <h3 className='text-zinc-50 font-bold text-2xl'>Swiss <br /> Wallet</h3>
+                </div>
 
-                        <div className='space-y-3'>
-                            
+                <div>
+                    <button>
+                        <CircleHelp className='size-10 text-zinc-300'/>
+                    </button>
+                </div>
+            </div>
 
-                           
-                                <div className='flex flex-col space-y-2'>
-                                    <h2 className='font-bold text-lg font-sans'>Nome completo</h2>
-                                    <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
-                                        type="text"
-                                        placeholder='Insira seu nome completo'    
-                                    />
-                                </div>
-
-                                <div className='flex flex-col space-y-2'>
-                                    <h2 className='font-bold text-lg font-sans'>Data de nascimento</h2>
-                                    <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
-                                        type="text"
-                                        placeholder='dd/mm/aaaa'    
-                                    />
-                                </div>
-
-                                <div className='flex flex-col space-y-2'>
-                                    <h2 className='font-bold text-lg font-sans'>E-mail</h2>
-                                    <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
-                                        type="text"
-                                        placeholder='Insira seu e-mail'    
-                                    />
-                                </div>
-
-                                <div className='flex flex-col space-y-2'>
-                                    <h2 className='font-bold text-lg font-sans'>CPF</h2>
-                                    <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
-                                        type="text"
-                                        placeholder='Insira seu CPF'    
-                                    />
-                                </div>
-
-                                <div className='flex flex-col space-y-2'>
-                                    <h2 className='font-bold text-lg font-sans'>Telefone</h2>
-                                    <input className='rounded-md border-2 border-zinc-400 p-3 w-auto'
-                                        type="text"
-                                        placeholder='Insira seu telefone'    
-                                    />
-                                </div>
+            
+            <RegisterStep1 />
 
 
 
-                            
-                        </div>
 
-                        <div className='flex justify-center'>
-                            <button className='bg-red-600 py-2 px-20 rounded-xl hover:bg-red-700'>
-                                <span className='text-zinc-50 font-bold text-lg'>Avançar</span>
-                            </button>
-                        </div>
-
-                    </div>
-                </form>
+            <div className='flex w-full justify-center'>
+                <p className='text-zinc-50 font-semibold'>© SwissWallet - 2024</p>
+            </div>
+        </div>
     )
 }
