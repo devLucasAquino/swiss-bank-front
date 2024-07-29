35d556449b6557de682b63bcf47a3c4ce68832aa
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-29 16:41:08 -0300

    organização de pastas

--- src/pages/register-page/index.tsx
+++ src/pages/register-page/index.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 import { CircleHelp, User } from "lucide-react";
 
-import { RegisterStep1 } from "./register-step1-modal";
-import { RegisterStep2 } from "./register-step-2-modal";
-import { RegisterStep3 } from "./register-step-3-modal";
-import { Link } from "react-router-dom";
+import { RegisterStep1 } from "./steps/register-step1-modal";
+import { RegisterStep2 } from "./steps/register-step-2-modal";
+import { RegisterStep3 } from "./steps/register-step-3-modal";
+import { CompleteRegister } from "./steps/complete-register-moda";
 
 
 
@@ -73,20 +73,7 @@ export function RegisterPage(){
             )}
 
             {completedRegister && (
-                <div className="flex flex-1 justify-center h-auto w-full">
-                    <div className='flex justify-center flex-col space-y-16 bg-white rounded-3xl w-[640px] px-9 py-14'>
-                        <h1 className="text-center text-3xl font-sans font-bold">Cadastro realizado com sucesso!</h1>
-                        <p className="text-center text-xl font-sans font-semibold">Você ja está pronto para aproveitar nossos produtos e ofertas, <br>
-                        </br> e esteja sempre preparado para novas promoções.</p>
-                        <div className='flex justify-center'>
-                            <Link to={`/`}>
-                                <button className='bg-red-600 py-2 px-20 rounded-xl hover:bg-red-700'>
-                                    <span className='text-zinc-50 font-bold text-lg'>Avançar</span>
-                                </button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+                <CompleteRegister />
             )}
 
             <div className='flex w-full justify-center'>
