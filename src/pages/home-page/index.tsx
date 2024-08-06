9fb6f0c264f631c5437d2351cc3fb3750af858f8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-06 18:28:27 -0300

    style: adjusted space beetween card AAPM and Favoritos

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -3,33 +3,33 @@ import { HomeHeader } from "../../components/home-header"
 
 export function HomePage(){
     return(
-        <div className="bg-slate-200 bg-opacity-50">
+        <div className="bg-slate-200 bg-opacity-50 space-y-20">
             <HomeHeader />
 
-            <main className="mr-24 ml-24 mt-20">
-                <div className="flex items-center justify-center space-x-36">
+            <main className="mr-40 ml-40">
+                <div className="flex items-center justify-between">
 
 
-                    <div className="w-auto h-32 space-y-3 p-5 rounded-xl bg-white shadow-xl">
+                    <div className="w-[620px] h-32 space-y-3 p-5 rounded-xl bg-white shadow-xl">
                         <div className="flex space-x-2 items-center">
                             <Heart className="size-7 font-bold "/>
                             <h2 className="text-2xl font-bold">AAPM</h2>
                         </div>
-                        <p className="w-96 font-medium">
+                        <p className="w-96 font-medium ml-10">
                             apoie na gestão da escola no alcance de suas metas e promoção a integração escola-comunidade.
                         </p>
                     </div>
 
 
 
-                    <div className="w-auto h-32 space-y-3 p-5 rounded-xl bg-white shadow-xl">
+                    <div className="w-[620px] h-32 space-y-3 p-5 rounded-xl bg-white shadow-xl">
 
                         <div className="flex space-x-2 items-center">
                             <Heart className="size-7 font-bold " />
                             <h2 className="text-2xl font-bold">Favoritos</h2>
                         </div>
                         <div className="flex justify-between">
-                            <p className="w-96 font-medium">
+                            <p className="w-96 font-medium ml-10">
                                 acessar os favoritos
                             </p>
                             <button>
@@ -41,6 +41,35 @@ export function HomePage(){
 
 
                 </div>
+
+
+
+                <div className="flex justify-center w-auto p-5 rounded-xl bg-white shadow-xl mt-20">
+                    <div className="w-full flex justify-start">
+                        <h1 className="text-3xl font-bold">Destaques - Loja</h1>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </main>
 
         </div>
